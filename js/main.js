c18b7e143816bcba4244e9b206116d3f5ba816fe
Tomasz Flakus <tomasz.flakus@gmail.com>
2020-03-05 21:51:02 +0100

    creating updateUI function and displaing data in browser

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
+const form = document.querySelector(".form");
 const output = document.querySelector(".outputField");
 
-const getCity = async () => {
-  const baseURI =
-    "https://api.openaq.org/v1/measurements?country=PL&limit=10&order_by=value&sort=desc";
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
-  const response = await fetch(baseURI);
+  const country = form.countries.value;
+  getCountry(country);
+
+  return country;
+});
+
+const getCountry = async country => {
+  const ask = `https://api.openaq.org/v1/measurements?country=${country}&limit=10&order_by=value&sort=desc`;
+  const response = await fetch(ask);
   const data = await response.json();
 
-  output.innerHTML = `
-  <h1>Wynik zapytania</h1>
-  `;
-};
-getCity();
+  console.log(country); // <-- zwraca undefine, dlaczego ?:(
+  console.log(data);
 
-// 1 - ogarnąć API openaq.
-// 2 - pobrać dane z api i wyświetplić na stronie `${}`.
-// 3 - pobrać dane do zapytania z inputa do zapytania API.
+  updateUi(data);
+  return data;
+};
+
+const updateUi = async data => {
+  for (let i = 0; i < data.results.length; i++) {
+    const html = document.createElement("li");
+    html.innerHTML = `
+         
+         <h4>Miasto: <a href="#">${data.results[i].city}</a> Pomiar zanieczyszczenia: <span>${data.results[i].value}</span></h4>
+       `;
+    output.append(html);
+  }
+};
+
+// 1 - ogarnąć API openaq. OK
+// 2 - pobrać dane z api i wyświetplić na stronie `${}`. OK
+// 3 - pobrać dane do zapytania z inputa do zapytania API. OK
 // 4 - stworzyć drugą funkcję z zapytaniem do API mediaWiki.
-// 5 - wyświetlić dane z API na tronie.
+// 5 - wyświetlić dane z API na tronie. OK* wyświetla tylko dane z openaq, trzeba dodtać API mediawiki!
